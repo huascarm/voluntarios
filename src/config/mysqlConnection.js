7d758ffcc20289a95fcb2dc03d02c544a0e1c639
huascarm <huascarm@gmail.com>
2019-05-09 18:31:08 -0400

    borre errores y arregle gitlab

--- src/config/mysqlConnection.js
+++ src/config/mysqlConnection.js
@@ -4,7 +4,7 @@ module.exports = () => {
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'S4prissa',
         database: 'voluntarios'
     })
 }
