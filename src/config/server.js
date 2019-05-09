c52042cf28268edc580961f6a765178285c580e5
huascarm <huascarm@gmail.com>
2019-05-09 17:23:52 -0400

    credenciales y puertos correctos

--- src/config/server.js
+++ src/config/server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 
 //iniciamos, configuramos y middleware
 let app = new Express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.use(bodyParser.urlencoded({extended: false}))
 
 module.exports = app;
