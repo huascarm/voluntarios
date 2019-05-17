91efa339e85f43c3b6e09af3d318515a07afd87a
huascarm <huascarm@gmail.com>
2019-05-17 14:51:05 -0400

    Listo para actualizar la base

--- src/app/routes/podiobd.js
+++ src/app/routes/podiobd.js
@@ -2,20 +2,19 @@ const PodioConfig =require( '../../config/podioConfig');
 const dbConnection =require( '../../config/mysqlConnection');
 const tse = dbConnection().tse;
 
-/*PodioConfig()
+PodioConfig()
 	.then((podio) => {
 		podio
 			.request('GET', '/item/app/21822217',{
-                limit: 15,
-                offset: 1000
+                limit: 500,
+                offset: 0
             })
 			.then(function(data) {
-                //console.log(data.items.length, 'items encontrados');
+                console.log(data.items.length, 'items encontrados');
                 let c=0;
                 data.items.forEach(element => {
-                  as(element.item_id)
                   
-                    /*tse.query( `UPDATE ciudadanos SET podio=1, itemIdPodio=${element.item_id} WHERE cedula =${element.title}` , (err, result) =>{
+                    tse.query( `UPDATE ciudadanos SET podio=1, itemIdPodio=${element.item_id} WHERE cedula =${element.title}` , (err, result) =>{
                         if(err){
                             console.log(err);
                         }else{
@@ -32,8 +31,8 @@ const tse = dbConnection().tse;
 		console.log(err);
     });
 
-
-  function as(id){*/
+/*
+  function as(id){
   PodioConfig()
 	.then((podio) => {
     let res = {};
@@ -49,66 +48,4 @@ const tse = dbConnection().tse;
   }).catch((err) => {
       console.log(err);
   })
-
-
-  function asigna(data){
-    let result = {}
-    data.forEach(campo => {
-      switch(campo.external_id) {
-        case 'lead-contact':
-          result.nombre = campo.values[0].value;
-          break;
-        case 'apellido':
-          result.apellido1= campo.values[0].value;
-          break;
-        case 'apellido-2':
-          result.apellido2= campo.values[0].value;
-          break;
-        case 'sexo':
-          result.sexo= campo.values[0].value.id
-          break;
-        case 'estado-civil-2':
-          result.civil= campo.values[0].value.id
-          break;
-        case 'provincia-2':
-          result.provincia= campo.values[0].value.text
-          break;
-        case 'canton-1':
-          result.canton= campo.values[0].value.text
-          break;
-        case 'canton-4':
-          if(!result.canton){
-            result.canton= campo.values[0].value.text
-          }
-          break;
-        case 'direccion':
-          result.direccion= campo.values[0].value
-          break;
-        case 'ocupacion-2':
-          result.ocupacion= campo.values[0].value
-          break;
-        case 'celular-3':
-          result.celular= campo.values[0].value
-          break;
-        case 'telefono-de-casa':
-          result.telefono1= campo.values[0].value
-          break;
-        case 'telefono-del-trabajo-3':
-          result.telefono2= campo.values[0].value
-          break;
-        case 'celular-de-familiar-o-amigo':
-          result.telFamiliar1= campo.values[0].value
-          break;
-        case 'email-2':
-          result.email1= campo.values[0].value
-          break;
-        case 'email2':
-          if(!result.email1){
-          result.email1= campo.values[0].value
-          }
-          break;
-      }
-    });
-
-    return result;
-  }
+*/
